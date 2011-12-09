dc7dd9e7bb9715a2595c22d74faad0346d985d3d
Evan Gamble <egamble@factual.com>
2011-12-09 01:19:49 -0800

    removed org.clojars. from the groupId

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.egamble/let-else "1.0.0"
+(defproject egamble/let-else "1.0.0"
   :description "Macro let? with :when and :else expressions in the bindings."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :url "https://github.com/egamble/let-else")
