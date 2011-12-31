07f0b573531e9858e442019784474f0a0f4213f7
Evan Gamble <egamble@factual.com>
2011-12-31 14:18:37 -0800

    added :delay to description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject egamble/let-else "1.0.0"
-  :description "Macro let? with :when and :else expressions in the bindings."
+  :description "Macro let? with :when, :else and :delay clauses in the bindings."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [useful "0.7.5-alpha4"]]
   :url "https://github.com/egamble/let-else")
