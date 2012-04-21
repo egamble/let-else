655473d20b441a95221fff3b22456615ba31869c
Evan Gamble <egamble@factual.com>
2012-04-20 18:51:56 -0700

    new version of useful lib

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject egamble/let-else "1.0.3"
   :description "Macro let? with :when, :is, :is-not, :else and :delay clauses in the bindings."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [useful "0.7.5-alpha4"]]
+                 [useful "0.7.10"]]
   :url "https://github.com/egamble/let-else")
